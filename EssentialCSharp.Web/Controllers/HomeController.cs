2b8d8c5c87531e83494970475bdb157a2e8970e8
ZackBWard <113388549+ZackBWard@users.noreply.github.com>
2022-09-20 15:36:34 -0700

    414 Clicking Next navigates to the next header not the next page (#453)
    https://github.com/IntelliTect/EssentialCSharpManuscript/issues/414
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

--- EssentialCSharp.Web/Controllers/HomeController.cs
+++ EssentialCSharp.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using EssentialCSharp.Web.Models;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace EssentialCSharp.Web.Controllers;
 
@@ -20,11 +21,13 @@ public class HomeController : Controller
     public IActionResult Index(string key)
     {
         string? html = null;
+        SiteMapping currentSiteMapping;
 
         // if no key (default case), then load up first page
         SiteMapping? siteMapping = SiteMapping.Find(key, _SiteMappings);
         if (siteMapping != null)
         {
+            currentSiteMapping = siteMapping;
             string filePath = Path.Combine(_HostingEnvironment.ContentRootPath, Path.Combine(siteMapping.PagePath));
             HtmlDocument doc = new();
             doc.Load(filePath);
@@ -38,6 +41,8 @@ public class HomeController : Controller
         {
             return RedirectToAction(nameof(Error), new { errorMessage = "Unexpected Exception; html being rendered is null" });
         }
+        ViewBag.NextPage = FlipPage(currentSiteMapping, true);
+        ViewBag.PreviousPage = FlipPage(currentSiteMapping, false);
         ViewBag.Contents = html;
         return View();
     }
@@ -49,6 +54,38 @@ public class HomeController : Controller
         return View();
     }
 
+    private string FlipPage(SiteMapping currentSiteMapping, bool next)
+    {
+        if(_SiteMappings.Count == 0)
+        {
+            return "";
+        }
+        int currentChapter = currentSiteMapping.ChapterNumber;
+        int currentPage    = currentSiteMapping.PageNumber;
+
+        int page = -1;
+        if (next)
+        {
+            page = 1;
+        }
+
+        SiteMapping? siteMap = _SiteMappings.FirstOrDefault(f => f.ChapterNumber == currentChapter && f.PageNumber == currentPage + page);
+        
+        if(siteMap == null)
+        {
+            if (next)
+            {
+                siteMap = _SiteMappings.FirstOrDefault(f => f.ChapterNumber == currentChapter + 1 && f.PageNumber == 1) ?? _SiteMappings.Last();
+            }
+            else
+            {
+                int? previousPage = _SiteMappings.LastOrDefault(f => f.ChapterNumber == currentChapter - 1)?.PageNumber;
+                siteMap = _SiteMappings.FirstOrDefault(f => f.ChapterNumber == currentChapter - 1 && f.PageNumber == previousPage) ?? _SiteMappings.First();
+            }
+        }
+        return $"{siteMap.Key}#{siteMap.AnchorId}";
+    }
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error(string? errorMessage = null)
     {
