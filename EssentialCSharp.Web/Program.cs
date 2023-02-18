e13847fdef5981d996873bf268078dd89a8286ec
Kevin B <Keboo@users.noreply.github.com>
2023-02-17 23:40:51 -0800

    .NET 7 upgrade (#753)
    This upgrades to .NET 7 / C#11 and addresses all new analyzer warnings.
    
    ## BEFORE MERGING
    
    - [ ]  Need to update the app service to use the .NET 7 runtime.

--- EssentialCSharp.Web/Program.cs
+++ EssentialCSharp.Web/Program.cs
@@ -1,46 +1,49 @@
 using EssentialCSharp.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-builder.Services.AddSingleton<IList<SiteMapping>>(sp =>
+internal sealed class Program
 {
-    IWebHostEnvironment _HostingEnvironment = sp.GetRequiredService<IWebHostEnvironment>();
-    string path = Path.Combine(_HostingEnvironment.ContentRootPath, "Chapters", "sitemap.json");
-    List<SiteMapping>? siteMappings = System.Text.Json.JsonSerializer.Deserialize<List<SiteMapping>>(File.OpenRead(path));
-    if (siteMappings is null)
+    private static void Main(string[] args)
     {
-        throw new InvalidOperationException("No table of contents found");
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddSingleton<IList<SiteMapping>>(sp =>
+        {
+            IWebHostEnvironment _HostingEnvironment = sp.GetRequiredService<IWebHostEnvironment>();
+            string path = Path.Combine(_HostingEnvironment.ContentRootPath, "Chapters", "sitemap.json");
+            List<SiteMapping>? siteMappings = System.Text.Json.JsonSerializer.Deserialize<List<SiteMapping>>(File.OpenRead(path));
+            if (siteMappings is null)
+            {
+                throw new InvalidOperationException("No table of contents found");
+            }
+            return siteMappings;
+        });
+
+        WebApplication app = builder.Build();
+        _ = app.Services.GetRequiredService<IList<SiteMapping>>();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.MapDefaultControllerRoute();
+
+        app.MapControllerRoute(
+            name: "slug",
+            pattern: "{*key}",
+            defaults: new { controller = "Home", action = "Index" });
+
+        app.Run();
     }
-    return siteMappings;
-});
-
-var app = builder.Build();
-_ = app.Services.GetRequiredService<IList<SiteMapping>>();
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAuthorization();
-
-app.UseEndpoints(endpoint =>
-{
-    endpoint.MapDefaultControllerRoute();
-    endpoint.MapControllerRoute(
-        name: "slug",
-        pattern: "{*key}",
-        defaults: new { controller = "Home", action = "Index" });
-});
-
-
-app.Run();
