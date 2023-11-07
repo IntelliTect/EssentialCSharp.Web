eb5d7838a29585810c8cd27937ef5adffe04d54a
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-07 17:50:24 +0100

    Remove more nullable disable

--- EssentialCSharp.Web/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
+++ EssentialCSharp.Web/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EssentialCSharp.Web.Areas.Identity.Pages.Account;
