4cd30cfbf7b97c6d1e3dbbba852bd30ec0071986
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-20 14:22:02 -0800

    Resolve TwoFactorAuthentication

--- EssentialCSharp.Web/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ EssentialCSharp.Web/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using EssentialCSharp.Web.Areas.Identity.Data;
+﻿using EssentialCSharp.Web.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,16 +7,16 @@ namespace EssentialCSharp.Web.Areas.Identity.Pages.Account.Manage;
 
 public class TwoFactorAuthenticationModel : PageModel
 {
-    private readonly UserManager<EssentialCSharpWebUser> _userManager;
-    private readonly SignInManager<EssentialCSharpWebUser> _signInManager;
-    private readonly ILogger<TwoFactorAuthenticationModel> _logger;
+    private readonly UserManager<EssentialCSharpWebUser> _UserManager;
+    private readonly SignInManager<EssentialCSharpWebUser> _SignInManager;
+    private readonly ILogger<TwoFactorAuthenticationModel> _Logger;
 
     public TwoFactorAuthenticationModel(
         UserManager<EssentialCSharpWebUser> userManager, SignInManager<EssentialCSharpWebUser> signInManager, ILogger<TwoFactorAuthenticationModel> logger)
     {
-        _userManager = userManager;
-        _signInManager = signInManager;
-        _logger = logger;
+        _UserManager = userManager;
+        _SignInManager = signInManager;
+        _Logger = logger;
     }
 
     public bool HasAuthenticator { get; set; }
@@ -33,33 +29,33 @@ public class TwoFactorAuthenticationModel : PageModel
     public bool IsMachineRemembered { get; set; }
 
     [TempData]
-    public string StatusMessage { get; set; }
+    public string? StatusMessage { get; set; }
 
     public async Task<IActionResult> OnGetAsync()
     {
-        EssentialCSharpWebUser user = await _userManager.GetUserAsync(User);
-        if (user == null)
+        EssentialCSharpWebUser? user = await _UserManager.GetUserAsync(User);
+        if (user is null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{_UserManager.GetUserId(User)}'.");
         }
 
-        HasAuthenticator = await _userManager.GetAuthenticatorKeyAsync(user) != null;
-        Is2faEnabled = await _userManager.GetTwoFactorEnabledAsync(user);
-        IsMachineRemembered = await _signInManager.IsTwoFactorClientRememberedAsync(user);
-        RecoveryCodesLeft = await _userManager.CountRecoveryCodesAsync(user);
+        HasAuthenticator = await _UserManager.GetAuthenticatorKeyAsync(user) != null;
+        Is2faEnabled = await _UserManager.GetTwoFactorEnabledAsync(user);
+        IsMachineRemembered = await _SignInManager.IsTwoFactorClientRememberedAsync(user);
+        RecoveryCodesLeft = await _UserManager.CountRecoveryCodesAsync(user);
 
         return Page();
     }
 
     public async Task<IActionResult> OnPostAsync()
     {
-        EssentialCSharpWebUser user = await _userManager.GetUserAsync(User);
-        if (user == null)
+        EssentialCSharpWebUser? user = await _UserManager.GetUserAsync(User);
+        if (user is null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{_UserManager.GetUserId(User)}'.");
         }
 
-        await _signInManager.ForgetTwoFactorClientAsync();
+        await _SignInManager.ForgetTwoFactorClientAsync();
         StatusMessage = "The current browser has been forgotten. When you login again from this browser you will be prompted for your 2fa code.";
         return RedirectToPage();
     }
