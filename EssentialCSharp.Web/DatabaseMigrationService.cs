a381064f5ac54f04d2bfde76205139a1f2abd463
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-02-24 08:49:57 -0800

    fix: Inject IServiceProvider instead of DbContext directly

--- EssentialCSharp.Web/DatabaseMigrationService.cs
+++ EssentialCSharp.Web/DatabaseMigrationService.cs
@@ -1,17 +1,21 @@
 ﻿using EssentialCSharp.Web.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EssentialCSharp.Web;
 
-public class DatabaseMigrationService : BackgroundService
+public class DatabaseMigrationService(IServiceProvider services) : BackgroundService
 {
-    public EssentialCSharpWebContext EssentialCSharpWebContext { get; }
-    public DatabaseMigrationService(EssentialCSharpWebContext essentialCSharpWebContext)
-    {
-        EssentialCSharpWebContext = essentialCSharpWebContext;
-    }
+    public IServiceProvider Services { get; } = services;
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await EssentialCSharpWebContext.Database.MigrateAsync(stoppingToken);
+        using IServiceScope scope = Services.CreateScope();
+        EssentialCSharpWebContext? context = scope.ServiceProvider.GetService<EssentialCSharpWebContext>();
+        if (context is null)
+        {
+            throw new InvalidOperationException($"EssentialCSharpWebContext not found for {nameof(DatabaseMigrationService)}");
+        }
+        await context.Database.MigrateAsync(stoppingToken);
     }
 }
