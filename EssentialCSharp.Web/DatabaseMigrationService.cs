2f60dd4f4e9577e14e128420f6d3de156bd4fbc5
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-02-24 09:02:26 -0800

    fix: Use IServiceFactory

--- EssentialCSharp.Web/DatabaseMigrationService.cs
+++ EssentialCSharp.Web/DatabaseMigrationService.cs
@@ -4,18 +4,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace EssentialCSharp.Web;
 
-public class DatabaseMigrationService(IServiceProvider services) : BackgroundService
+public class DatabaseMigrationService(IServiceScopeFactory services) : BackgroundService
 {
-    public IServiceProvider Services { get; } = services;
+    public IServiceScopeFactory Services { get; } = services;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         using IServiceScope scope = Services.CreateScope();
-        EssentialCSharpWebContext? context = scope.ServiceProvider.GetService<EssentialCSharpWebContext>();
-        if (context is null)
-        {
-            throw new InvalidOperationException($"EssentialCSharpWebContext not found for {nameof(DatabaseMigrationService)}");
-        }
+        EssentialCSharpWebContext? context = scope.ServiceProvider.GetRequiredService<EssentialCSharpWebContext>() 
+            ?? throw new InvalidOperationException($"EssentialCSharpWebContext not found for {nameof(DatabaseMigrationService)}");
         await context.Database.MigrateAsync(stoppingToken);
     }
 }
