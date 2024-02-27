a2e67825b913d0c0fe9c92afb0964a667f096c17
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-02-26 23:24:21 -0800

    test: Give captcha tests less requirements from Program.cs

---
+++ EssentialCSharp.Web/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using EssentialCSharp.Web.Services;
+
+namespace EssentialCSharp.Web.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddCaptchaService(this IServiceCollection services, IConfigurationSection CaptchaOptions)
+    {
+        services.Configure<CaptchaOptions>(CaptchaOptions);
+        services.AddSingleton<ICaptchaService, CaptchaService>();
+        services.AddHttpClient("hCaptcha", c =>
+        {
+            c.BaseAddress = new Uri("https://api.hcaptcha.com");
+        });
+    }
+}
