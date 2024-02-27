b2262afc0ce63bc418ea32930af9c4df408b3d5f
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-02-26 22:09:06 -0800

    fix: Get HCaptcha to work properly

--- EssentialCSharp.Web/Models/HCaptchaResult.cs
+++ EssentialCSharp.Web/Models/HCaptchaResult.cs
@@ -2,11 +2,31 @@
 
 namespace EssentialCSharp.Web.Models;
 
+// https://docs.hcaptcha.com/#verify-the-user-response-server-side
+//{
+//   "success": true|false,     // is the passcode valid, and does it meet security criteria you specified, e.g. sitekey?
+//   "challenge_ts": timestamp, // timestamp of the challenge (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+//   "hostname": string,        // the hostname of the site where the challenge was solved
+//   "credit": true|false,      // optional: deprecated field
+//   "error-codes": [...]       // optional: any error codes
+//   "score": float,            // ENTERPRISE feature: a score denoting malicious activity.
+//   "score_reason": [...]      // ENTERPRISE feature: reason(s) for score.
+//}
 public class HCaptchaResult
 {
+
+    [JsonPropertyName("success")]
     public bool Success { get; set; }
-    public DateTime ChallengeTimeStamp { get; set; }
+
+    [JsonPropertyName("challenge_ts")]
+    public DateTimeOffset ChallengeTimeStamp { get; set; }
+
+    [JsonPropertyName("hostname")]
     public string Hostname { get; set; } = string.Empty;
+
+    [JsonPropertyName("credit")]
+    public bool Credit { get; set; }
+
     [JsonPropertyName("error-codes")]
-    public List<string> ErrorCodes { get; set; } = new();
+    public string[]? ErrorCodes { get; set; }
 }
