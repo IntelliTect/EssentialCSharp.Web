cdd0256ea33e8fa2bb8e09a26bb658e1ebc91685
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-05-10 09:21:25 -0700

    build(deps): Bump deps and fix errors (#452)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- EssentialCSharp.Web.Tests/SiteMappingTests.cs
+++ EssentialCSharp.Web.Tests/SiteMappingTests.cs
@@ -4,41 +4,47 @@ namespace EssentialCSharp.Web.Tests;
 
 public class SiteMappingTests
 {
-    static SiteMapping HelloWorldSiteMapping { get; } = new(Key: "hello-world",
-            PagePath:
+    static SiteMapping HelloWorldSiteMapping => new(
+            key: "hello-world",
+            pagePath:
             [
                 "Chapters",
                 "01",
                 "Pages",
                 "01.html"
             ],
-            ChapterNumber: 1,
-            PageNumber: 1,
-            ChapterTitle: "Introducing C#",
-            RawHeading: "Introduction",
-            AnchorId: "hello-world",
-            IndentLevel: 0);
-    static SiteMapping CSyntaxFundamentalsSiteMapping { get; } = new(Key: "c-syntax-fundamentals",
-            PagePath:
+            chapterNumber: 1,
+            pageNumber: 1,
+            chapterTitle: "Introducing C#",
+            rawHeading: "Introduction",
+            anchorId: "hello-world",
+            indentLevel: 0
+    );
+
+    static SiteMapping CSyntaxFundamentalsSiteMapping => new(
+            key: "c-syntax-fundamentals",
+            pagePath:
             [
                 "Chapters",
                 "01",
                 "Pages",
                 "02.html"
             ],
-            ChapterNumber: 1,
-            PageNumber: 2,
-            ChapterTitle: "Introducing C#",
-            RawHeading: "C# Syntax Fundamentals",
-            AnchorId: "c-syntax-fundamentals",
-            IndentLevel: 2);
+            chapterNumber: 1,
+            pageNumber: 2,
+            chapterTitle: "Introducing C#",
+            rawHeading: "C# Syntax Fundamentals",
+            anchorId: "c-syntax-fundamentals",
+            indentLevel: 2
+    );
+
     public static List<SiteMapping> GetSiteMap()
     {
         return
         [
             HelloWorldSiteMapping,
             CSyntaxFundamentalsSiteMapping
-    ];
+        ];
     }
 
     [Fact]
@@ -46,27 +52,30 @@ public class SiteMappingTests
     {
         SiteMapping? foundSiteMap = GetSiteMap().Find("hello-world#hello-world");
         Assert.NotNull(foundSiteMap);
-        Assert.Equal(HelloWorldSiteMapping, foundSiteMap!);
+        Assert.Equal(HelloWorldSiteMapping, foundSiteMap);
     }
+
     [Fact]
     public void FindCSyntaxFundamentalsWithSpacesReturnsCorrectSiteMap()
     {
         SiteMapping? foundSiteMap = GetSiteMap().Find("C# Syntax Fundamentals");
         Assert.NotNull(foundSiteMap);
-        Assert.Equal(CSyntaxFundamentalsSiteMapping, foundSiteMap!);
+        Assert.Equal(CSyntaxFundamentalsSiteMapping, foundSiteMap);
     }
+
     [Fact]
     public void FindCSyntaxFundamentalsWithSpacesAndAnchorReturnsCorrectSiteMap()
     {
         SiteMapping? foundSiteMap = GetSiteMap().Find("C# Syntax Fundamentals#hello-world");
         Assert.NotNull(foundSiteMap);
-        Assert.Equal(CSyntaxFundamentalsSiteMapping, foundSiteMap!);
+        Assert.Equal(CSyntaxFundamentalsSiteMapping, foundSiteMap);
     }
+
     [Fact]
     public void FindCSyntaxFundamentalsSanitizedWithAnchorReturnsCorrectSiteMap()
     {
         SiteMapping? foundSiteMap = GetSiteMap().Find("c-syntax-fundamentals#hello-world");
         Assert.NotNull(foundSiteMap);
-        Assert.Equal(CSyntaxFundamentalsSiteMapping, foundSiteMap!);
+        Assert.Equal(CSyntaxFundamentalsSiteMapping, foundSiteMap);
     }
 }
--- EssentialCSharp.Web/Extensions/SiteMappingListExtensions.cs
+++ EssentialCSharp.Web/Extensions/SiteMappingListExtensions.cs
@@ -2,9 +2,18 @@
 
 public static class SiteMappingListExtensions
 {
-    public static SiteMapping? Find(this IList<SiteMapping> siteMappings, string key)
+    /// <summary>
+    /// Finds a site mapping based on a key.
+    /// </summary>
+    /// <param name="siteMappings">IList of SiteMappings</param>
+    /// <param name="key">If null, uses the first key in the list</param>
+    /// <returns>If found, the site mapping that matches the key, otherwise null.</returns>
+    public static SiteMapping? Find(this IList<SiteMapping> siteMappings, string? key)
     {
-        key = siteMappings[0].Key;
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            return siteMappings.FirstOrDefault();
+        }
         foreach (string? potentialMatch in key.GetPotentialMatches())
         {
             if (siteMappings.FirstOrDefault(x => x.Key == potentialMatch) is { } siteMap)
