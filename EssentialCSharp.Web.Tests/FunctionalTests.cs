8715da5aa4dbc7ef370fce9b2b8cf069ef7ba080
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-06-11 11:28:28 -0700

    fix: Unskip tests (#458)

--- EssentialCSharp.Web.Tests/FunctionalTests.cs
+++ EssentialCSharp.Web.Tests/FunctionalTests.cs
@@ -5,10 +5,10 @@ namespace EssentialCSharp.Web.Tests;
 public class FunctionalTests
 {
     [Theory]
-    [InlineData("/", Skip = "CI Needs to get the key vault values for this to work")]
-    [InlineData("/hello-world", Skip = "CI Needs to get the key vault values for this to work")]
-    [InlineData("/hello-world#hello-world", Skip = "CI Needs to get the key vault values for this to work")]
-    //Skip this test
+    [InlineData("/")]
+    [InlineData("/hello-world")]
+    [InlineData("/hello-world#hello-world")]
+    [InlineData("/guidelines")]
     public async Task WhenTheApplicationStarts_ItCanLoadLoadPages(string relativeUrl)
     {
         using WebApplicationFactory factory = new();
@@ -17,5 +17,16 @@ public class FunctionalTests
         using HttpResponseMessage response = await client.GetAsync(relativeUrl);
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task WhenTheApplicationStarts_NonExistingPage_GivesCorrectStatusCode()
+    {
+        using WebApplicationFactory factory = new();
+
+        HttpClient client = factory.CreateClient();
+        using HttpResponseMessage response = await client.GetAsync("/non-existing-page1234");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 }
