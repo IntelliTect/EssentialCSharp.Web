3cc0b4cb21301f794a6264be906efd4cce13c940
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2025-02-17 14:05:20 -0800

    Migration

---
+++ EssentialCSharp.Web/Migrations/20250217215739_ReferralCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EssentialCSharp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReferralCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReferralCount",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferralCount",
+                table: "AspNetUsers");
+        }
+    }
+}
