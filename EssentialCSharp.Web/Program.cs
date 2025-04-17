334d38a7bf9bd5110246405074b9d8d4af685232
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2025-04-17 10:38:43 -0700

    another test

--- EssentialCSharp.Web/Program.cs
+++ EssentialCSharp.Web/Program.cs
@@ -23,6 +23,12 @@ public partial class Program
         {
             options.ForwardedHeaders =
                 ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+
+            // Only loopback proxies are allowed by default.
+            // Clear that restriction because forwarders are enabled by explicit 
+            // configuration.
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
         });
 
         ConfigurationManager configuration = builder.Configuration;
