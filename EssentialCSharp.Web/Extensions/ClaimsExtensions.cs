3e54799a9f1e591198f4bad31652cedbfee45fe8
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2025-07-29 12:28:06 -0700

    fix: Add Referral Id to controller (#764)

--- EssentialCSharp.Web/Extensions/ClaimsExtensions.cs
+++ EssentialCSharp.Web/Extensions/ClaimsExtensions.cs
@@ -13,5 +13,16 @@ public static class ClaimsExtensions
     {
         return claims.FirstOrDefault(claim => claim.Type == ReferrerIdClaimType)?.Value;
     }
+
+    /// <summary>
+    /// Gets the referral ID from the current user's claims in the HttpContext
+    /// </summary>
+    /// <param name="httpContext">The HttpContext to get the referral ID from</param>
+    /// <returns>The referral ID if found, otherwise null</returns>
+    public static string? GetReferrerId(this HttpContext? httpContext)
+    {
+        return httpContext?.User?.GetReferrerId();
+    }
+
     public const string ReferrerIdClaimType = "ReferrerId";
 }
